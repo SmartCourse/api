513ffa3ecbd9fb83bf03098982a656a4d4744bab
Nuno Das Neves <ndasneves@gmail.com>
2019-02-12 20:01:44 +1100

    logs

--- docker.sh
+++ docker.sh
@@ -13,14 +13,13 @@ docker run \
     --env 'ACCEPT_EULA=Y' \
     --env "SA_PASSWORD=$LOCAL_SQL_PASSWORD" \
     -p 1433:1433 \
-    -d microsoft/mssql-server-linux:2017-latest 
+    -d microsoft/mssql-server-linux:2017-latest
 
-sleep 20
-docker ps -a
-sleep 30
+sleep 10
+docker logs $DOCKER_NAME
 
 docker exec -it $DOCKER_NAME /opt/mssql-tools/bin/sqlcmd \
    -S localhost,1433 -U SA -P "$LOCAL_SQL_PASSWORD" \
    -Q 'CREATE DATABASE testdb'
- 
+
 # really this should be a dockerfile as with all db config \_(* *)_/
