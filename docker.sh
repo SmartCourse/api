9e01a7014e7d7b2698432b34b76bcf7d3e133431
Alex Hinds <ahinds@atlassian.com>
2019-04-11 15:40:06 +1000

    moved some of config to docker-compose

--- docker.sh
+++ docker.sh
@@ -5,20 +5,15 @@ DOCKER_NAME='smartcourse-testing'
 docker kill $DOCKER_NAME
 docker rm $DOCKER_NAME
 
+# bail out if any errors
 set -ex
 
-# docker build --tag $DOCKER_NAME .
-docker run \
-    --name $DOCKER_NAME \
-    --env 'ACCEPT_EULA=Y' \
-    --env "SA_PASSWORD=$LOCAL_SQL_PASSWORD" \
-    -p 1433:1433 \
-    -d microsoft/mssql-server-linux:2017-latest
+# start the container using compose config
+SA_PASSWORD="$LOCAL_SQL_PASSWORD" docker-compose up -d
 
-sleep 10
-
-docker exec -it $DOCKER_NAME /opt/mssql-tools/bin/sqlcmd \
-   -S localhost,1433 -U SA -P "$LOCAL_SQL_PASSWORD" \
+# initdb
+docker exec -it $DOCKER_NAME sqlcmd \
+   -S localhost,1433 \
+   -U SA \
+   -P "$LOCAL_SQL_PASSWORD" \
    -Q 'CREATE DATABASE testdb'
-
-# really this should be a dockerfile as with all db config \_(* *)_/
